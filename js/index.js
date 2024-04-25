16a280b0405ca018ba5dfccf79f71bcfa3210651
Gabriel Aguiar Queiroz <108091647+Bielzera28@users.noreply.github.com>
2024-04-24 21:12:55 -0300

    Rotas e paginas

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import { Router } from "./router.js"
 
 const router = new Router()
-router.add("/", "/pages/home.html")
-router.add("/about", "/pages/about.html")
-router.add("/contact", "/pages/contact.html")
+router.add("/", "/pages/home/home.html")
+router.add("/universe", "/pages/universe/universe.html")
+router.add("/explorer", "/pages/explorer/explorer.html")
 router.add(404, "/pages/404.html")
 
 router.handle()
